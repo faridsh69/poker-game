9cd633e4aac2ed69b7fed188eeacebd7f984b6ed
Farid Shahidi <shahidi@innoscripta.com>
2024-01-11 11:32:31 +0100

    fixed build version

---
+++ front/dist/assets/AdminShow-d55d2592.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-a7a0db46.js";const s=()=>o.jsx("div",{children:"show"});export{s as default};
--- front/dist/assets/AdminShow-e5f61eb8.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-7d814c2d.js";const s=()=>o.jsx("div",{children:"show"});export{s as default};
