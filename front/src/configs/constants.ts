c3ac4dff971b88f55b28aa165ced2f474b3bf5a3
Farid Shahidi <shahidi@innoscripta.com>
2024-01-15 13:08:25 +0100

    fixed v 61

--- front/src/configs/constants.ts
+++ front/src/configs/constants.ts
@@ -10,7 +10,7 @@ export const LOCAL_STORAGE_ACCESS_TOKEN_KEY = 'access_token'
 export const LOCAL_STORAGE_AUTH_USER_EMAIL = 'auth_email'
 
 export const META_TAGS = {
-  title: 'Royal Poker',
+  title: 'Royal Poker V 0.61',
   description: 'Royal Poker is easy and safe',
   keywords: 'react, boilerplate',
   image: 'vite.svg',
