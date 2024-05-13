ffabafb74d3479d76e4d9ed9c905e6e5923f4b39
Farid Shahidi <shahidi@innoscripta.com>
2024-05-13 18:02:58 +0200

    Fixed

--- back/src/seeders/sources/tables.seeder.ts
+++ back/src/seeders/sources/tables.seeder.ts
@@ -1,25 +1,25 @@
-import { TABLE_PASOORS } from 'src/configs/serverPokerConstants'
+// import { TABLE_PASOORS } from 'src/configs/serverPokerConstants'
 import { Table } from 'src/models/table.entity'
 
 export const TABLES_SEEDER = [
-  {
-    id: 1,
-    title: 'Holdem 1$ 2$, Buy in: 100$ - 500$',
-    pasoor: TABLE_PASOORS.holdem,
-    blinds_small: 1,
-    blinds_big: 2,
-    buyin_min: 100,
-    buyin_max: 500,
-    seats: 6,
-  },
-  {
-    id: 2,
-    title: 'Holdem 5$ 10$, Buy in: 500$ - 1000$',
-    pasoor: TABLE_PASOORS.holdem,
-    blinds_small: 5,
-    blinds_big: 10,
-    buyin_min: 500,
-    buyin_max: 2000,
-    seats: 9,
-  },
+  // {
+  //   id: 1,
+  //   title: 'Holdem 1$ 2$, Buy in: 100$ - 500$',
+  //   pasoor: TABLE_PASOORS.holdem,
+  //   blinds_small: 1,
+  //   blinds_big: 2,
+  //   buyin_min: 100,
+  //   buyin_max: 500,
+  //   seats: 6,
+  // },
+  // {
+  //   id: 2,
+  //   title: 'Holdem 5$ 10$, Buy in: 500$ - 1000$',
+  //   pasoor: TABLE_PASOORS.holdem,
+  //   blinds_small: 5,
+  //   blinds_big: 10,
+  //   buyin_min: 500,
+  //   buyin_max: 2000,
+  //   seats: 9,
+  // },
 ] as Table[]
